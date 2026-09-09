ec2a53f2ef756fe88bad69d8b1344af5da56f34a
Matin <kazutomartin20@gmail.com>
2026-09-09 18:05:45 +0330

    Add ablation script for focal loss hyperparameter dynamic tunning

--- train_self_supervised.py
+++ train_self_supervised.py
@@ -20,7 +20,6 @@ torch.backends.cuda.enable_mem_efficient_sdp(False)
 torch.backends.cuda.enable_math_sdp(True)  # force the reference math backend everywhere
 
 
-import fdafdafd
 torch.manual_seed(0)
 np.random.seed(0)
 
